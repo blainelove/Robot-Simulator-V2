f15d87fac16582d4bf793277e13000bf75ccf2ef
William Bugenis <wbugenis@gmail.com>
2021-02-25 12:03:46 -0500

    Done

--- index.js
+++ index.js
@@ -3,4 +3,51 @@ document.addEventListener("DOMContentLoaded", function() {
   init();
 
   // ADD CODE HERE!
+  const movesUl = document.querySelector('ul#moves-container')
+
+  document.addEventListener("keydown", function(event) {
+    const li =  document.createElement('li')
+
+    switch(event.key){
+      case "ArrowRight":
+        li.innerHTML = "right"
+        movesUl.append(li)
+        break;
+      case "ArrowLeft":
+        li.innerHTML = "left"
+        movesUl.append(li)
+        break;
+      case "ArrowUp":
+        li.innerHTML = "up"
+        movesUl.append(li)
+        break;
+      case "ArrowDown":
+        li.innerHTML = "down"
+        movesUl.append(li)
+        break;
+    }
+
+  })
+
+  const moveButton = document.querySelector("button#move-button")
+
+  function doMoves(){
+    const moves = movesUl.querySelectorAll('li')
+
+    for(let j = 0; j < movesUl.children.length; j++ ) {
+      setTimeout(function(){
+        move(movesUl.children[0].innerHTML)
+        movesUl.children[0].remove()
+       }, 500 * j)
+      
+      }
+  }
+
+  moveButton.addEventListener('click', doMoves)
+
+  movesUl.addEventListener('click', function(event) {
+    console.log(event.target)
+    event.target.remove()
+  })
+
 });
